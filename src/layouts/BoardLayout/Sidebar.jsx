f14292753ec633ebca9089608106949927ae60dd
Arcee69 <olawalearcee@gmail.com>
2024-08-09 21:56:38 +0100

    Hot Fix

--- src/layouts/BoardLayout/Sidebar.jsx
+++ src/layouts/BoardLayout/Sidebar.jsx
@@ -27,28 +27,28 @@ const Sidebar = () => {
         
         <div className='flex flex-col items-center gap-4'>
             <div 
-                onClick={() => {userData?.kyc_status === "pending" ? null : navigate("/dashboard"), window.scrollTo(0, 0)}} 
+                onClick={() => {userData?.kyc_status === "pending" || userData?.kyc_status === "applied" ? null : navigate("/dashboard"), window.scrollTo(0, 0)}} 
                 className={`${location?.pathname === "/dashboard"  ? "bg-[#F4F6FB]" : ""} w-[222px] h-[48px] rounded-lg  gap-2 flex items-center group cursor-pointer transition-all duration-300 p-[16px] hover:bg-[#ECFFF6]`}
             >
                 <RxDashboard className={`${location.pathname === "/dashboard" ? "text-[#009254]" : "text-[#292D32]"} w-5 h-5  group-hover:text-[#111827] `}/>
                 <p className="text-[#111827] font-medium font-inter">Dashboard</p>
             </div>
             <div 
-                onClick={() => {userData?.kyc_status === "pending" ? null : navigate("/inventory"), window.scrollTo(0, 0)}} 
+                onClick={() => {userData?.kyc_status === "pending" || userData?.kyc_status === "applied" ? null : navigate("/inventory"), window.scrollTo(0, 0)}} 
                 className={`${location?.pathname === "/inventory"  ? "bg-[#F4F6FB]" : ""} w-[222px] h-[48px] rounded-lg  gap-2 flex items-center group cursor-pointer transition-all duration-300 p-[16px] hover:bg-[#ECFFF6]`}
             >
                 <FaUserGroup  className={`${location.pathname === "/inventory" ? "text-[#009254]" : "text-[#292D32]"} w-5 h-5  group-hover:text-[#111827] `}/>
                 <p className="text-[#111827] font-medium font-inter">Inventory</p>
             </div>
             <div 
-                onClick={() => {userData?.kyc_status === "pending" ? null : navigate("/orders"), window.scrollTo(0, 0)}} 
+                onClick={() => {userData?.kyc_status === "pending" || userData?.kyc_status === "applied" ? null : navigate("/orders"), window.scrollTo(0, 0)}} 
                 className={`${location?.pathname === "/orders"  ? "bg-[#F4F6FB]" : ""} w-[222px] h-[48px] rounded-lg  gap-2 flex items-center group cursor-pointer transition-all duration-300 p-[16px] hover:bg-[#ECFFF6]`}
             >
                 <CiShoppingCart className={`${location.pathname === "/orders" ? "text-[#009254]" : "text-[#292D32]"} w-5 h-5  group-hover:text-[#111827] `}/>
                 <p className="text-[#111827] font-medium whitespace-nowrap font-inter">Orders</p>
             </div>
             <div 
-                onClick={() => {userData?.kyc_status === "pending" ? null : navigate("/sales"), window.scrollTo(0, 0)}} 
+                onClick={() => {userData?.kyc_status === "pending" || userData?.kyc_status === "applied" ? null : navigate("/sales"), window.scrollTo(0, 0)}} 
                 className={`${location?.pathname === "/sales"  ? "bg-[#F4F6FB]" : ""} w-[222px] h-[48px] rounded-lg  gap-2 flex items-center group cursor-pointer transition-all duration-300 p-[16px] hover:bg-[#ECFFF6]`}
             >
                 <TbReceipt className={`${location.pathname === "/sales" ? "text-[#009254]" : "text-[#292D32]"} w-5 h-5  group-hover:text-[#111827] `}/>
